462833dda7b84654e282f3619dda7009ba415ec5
Nimota Yusuf <nimota2001@gmail.com>
2025-05-26 03:39:55 -0700

    Second Commit

---
+++ custom_module.py
@@ -0,0 +1,18 @@
+def add(x,y):
+    return   x + y
+#print(add(3,4))
+
+def square(x):
+    return x*x
+#print(square(5))
+
+def subtract(a,b):
+    try:
+        result = a - b
+    except TypeError:
+        return('Cannot accept string arguments, only numbers')
+    else:
+        print(result)
+#print(subtract('c' , 'd'))
+
+  
